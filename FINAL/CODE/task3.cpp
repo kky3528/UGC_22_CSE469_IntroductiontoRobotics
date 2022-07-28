c409367a296e1b8862f891c86be6440ba2396c90
rvis-imac <rvis-imac@rvis-imacui-iMac.local>
2022-07-28 16:35:07 +0900

    add
    Former-commit-id: 0fdf80695e700ea9c15be758993a76a93a95942b

---
+++ FINAL/CODE/task3.cpp
@@ -0,0 +1,73 @@
+#include "../include/final/final.h"
+
+int main(int argc, char** argv) {
+
+  //terminal_init();
+  //set_terminal_raw_mode();
+
+  ros::init(argc, argv, "TASK3");
+  groupinit();
+  ros::NodeHandle node;
+
+  //setting for velocity
+  vel_pub = node.advertise<geometry_msgs::Twist>("cmd_vel", 1, true);
+  geometry_msgs::Twist twist;
+
+  //setting for position
+  curpos_sub = node.subscribe("odom",100,update_cur_pos);
+
+  printText();
+
+  //start code for task
+  printf("START!!!\n");
+
+  move_pos(pos_7, node);
+  if (moving) moving = false;
+  else return -1;
+
+  move_pos(pos_8, node);
+  if (moving) moving = false;
+  else return -1;
+
+  head_object(node);
+  if (detected) detected = false;
+  else return -1;
+
+  head_tracking(node);
+  if (approach) approach = false;
+  else return -1;
+
+  hold_or_return(1);
+
+  move_reverse(node);
+/*  move_pos(pos_8, node);
+  if (moving) moving = false;
+  else return -1;
+
+  move_pos(pos_7, node);
+  if (moving) moving = false;
+  else return -1;
+
+  move_pos(pos_1, node);
+  if (moving) moving = false;
+  else return -1;
+
+  hold_or_return(2);
+*/
+  printf("END!!!\n");
+  //end code for task
+ 
+
+  //finalization
+  gripper(gr_finish);
+  position(ar_home, 5.0);
+  arm = ar_home;
+  hand = gr_finish;
+
+  printText();
+
+  //set_terminal_original_mode();
+  //terminal_done();
+
+  return 0;
+}
